e222451b4a4b08518b7a86e3390be9ee6d03b4ea
SirNeon618 <SirNeon618@users.noreply.github.com>
2014-11-05 21:20:50 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ config = {
 	'description': 'Source code for /u/RedditAnalysisBot',
 	'author': '/u/SirNeon',
 	'url': 'https://github.com/SirNeon618/SubredditAnalysis',
-	'download_url': 'https://github.com/SirNeon618/SubredditAnalysis/archive/master.zip',
+	'download_url': 'https://github.com/SirNeon618/SubredditAnalysis/archive/1.0.zip',
 	'version': '1.0',
 	'install_requires': ['praw', 'requests', 'simpleconfigparser'],
 	'packages': [],
